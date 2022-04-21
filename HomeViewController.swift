1db4d44eedb89bf7c774b749600b29b651ec9a00
Sudang <80115601+suyeon100@users.noreply.github.com>
2022-04-21 18:47:27 +0900

    tableViewDelete

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -7,21 +7,27 @@
 
 import UIKit
 
+
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-  
     @IBOutlet weak var tableView: UITableView!
     
+
+    
+    var info:[UIColor] = [
+        .orange, .yellow, .brown, .green, .gray, .lightGray, .systemPink, .red
+    ]
     var user:[String] = ["nana_b", "developer", "suzan", "hoya", "love_ing_", "jully", "jisuss","biggg"]
     var content: [String] = [
-        "안녕하세요 ! 이번 주말에 성수 놀러가는데 \n 추천해주실 맛집, 카페 있나요??? ",
-        "카공하기 좋은 조용하고 넓은 카페를 찾고있습니다. 홍대역에 추천할만한 카페가 있을까요 !",
+
+        "이번 주말에 성수 놀러가는데 추천해주실 맛집,카페 있나요?? ",
+        "카공하기 좋은 조용하고 넓은 카페 추천해주세요😆 ",
         "친구들과 한강가기로 했는데 추천할만한 곳이 있나요?",
         "국내 여행가기 좋은 장소 추천해주세요 !",
         "요즘 핫한 장소가 어디에요??",
         "어른들과 함께가기 좋은 맛집이 어디인가요?",
-        "아이와 함께 갈 수 있는 맛집, 카페 추천해주세요 ~~~",
-        "서울에 애완동물과 함께 갈 수 있는 카페가 있나요?"
+        "아이와 함께 갈 수 있는 맛집, 카페 추천해주세요~~~",
+        "서울에 반려견과 함께 갈 수 있는 카페가 있나요?"
     ]
    
     
@@ -33,16 +39,32 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath)
         as! HomeTableViewCell
         
+        
+        cell.user.backgroundColor = info[indexPath.row]
         cell.tooLabel.text = user[indexPath.row]
         cell.contentLabel.text = content[indexPath.row]
-        
+        if cell.contentLabel.adjustsFontSizeToFitWidth == false {
+            cell.contentLabel.adjustsFontSizeToFitWidth = true
+        }
+
         return cell
     }
-    
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+            if editingStyle == .delete {
+                
+                user.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                
+            } else if editingStyle == .insert {
+
+            }
+        }
+
     
 
     override func viewDidLoad() {
@@ -64,12 +86,12 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         navigationItem.rightBarButtonItem = item
         
-
     }
     
   @objc func addFeed() {
-        
+      
     }
-}
 
+    
+}
 
